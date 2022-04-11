c4ea34b73d24230f915a9b589612a372ceffee3d
PatrickBanez <73153494+PatrickBanez@users.noreply.github.com>
2022-04-11 10:37:21 -0700

    writing/appending to a file

--- FileReadingTest/fileio.py
+++ FileReadingTest/fileio.py
@@ -1,11 +1,20 @@
-filePath = "C:\\Users\\p_tri\\Desktop\\Python\\FileReadingTest\\Latin-Lipsum.txt"
-
-with open(filePath, 'r') as file:
-    text = file.read()
+readPath = "C:\\Users\\p_tri\\Desktop\\Python\\FileReadingTest\\Latin-Lipsum.txt"
+# read a file
+with open(readPath, 'r') as rfile:
+    text = rfile.read()
 
 text = text.split(" ")
-for word in text:
-    if(len(word) == 5 and word[-1] != ","):
-        print(word)
+
+writePath = "C:\\Users\\p_tri\\Desktop\\Python\\FileReadingTest\\Write-Output-Test.txt"
+# write to a file
+with open(writePath, 'w') as wfile:
+    for word in text:
+        if(len(word) == 5 and word[-1] != ','):
+            wfile.write(word + "\n")
+
+# append to a file
+with open(writePath, 'a') as afile:
+    for x in range(100):
+        afile.write(str(x + 1) + "\n")
 
 # print(text)
